c28cdcffb0f6bc719c0c0af35f177c3ab8f44f99
Valerian_He <1826906282@qq.com>
2023-08-25 11:33:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,11 +14,3 @@ def format_json_file(file):
         f=open(file,"r+")
         content=f.read()
         if len(content) == 0 or content[0] == '[':
-            print(" {} doesn't need formatting".format(file))
-            f.close()
-            return
-        time.sleep(1)
-        print("test")
-
-format_json_file("./out.json")
-open_json_file("./out.json")
